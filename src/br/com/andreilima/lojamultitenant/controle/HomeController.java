c59769d1f6ed2251cde2587e2a98fdf0a5860350
Andrei <andrei.lima@usinasaodomingos.com.br>
2013-07-05 02:07:08 -0300

    primeiro commit

---
+++ src/br/com/andreilima/lojamultitenant/controle/HomeController.java
@@ -0,0 +1,15 @@
+package br.com.andreilima.lojamultitenant.controle;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
+
+
+
+@Resource
+public class HomeController {
+	
+	@Get("home")
+	public void home(){
+		
+	}
+}
